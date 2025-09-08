69552d859b94cdf5fe54256c72d2e966a1499bf9
Sofia Borovskaia <sofiaborovskaia@gmail.com>
2025-09-08 15:43:38 +0100

    feat: add hat model

---
+++ src/components/models/HatModel.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { useGLTF } from '@react-three/drei';
+import { Matrix4, Vector3, Quaternion } from 'three';
+
+const HatModel = ({ transformMatrix }) => {
+  const { scene } = useGLTF('/assets/models/hat.glb');
+  const customScale = 24;
+
+  useEffect(() => {
+    if (transformMatrix) {
+      const m = new Matrix4().fromArray(transformMatrix);
+      const position = new Vector3();
+      const rotation = new Quaternion();
+      const scale = new Vector3();
+
+      m.decompose(position, rotation, scale);
+
+      scene.position.copy(position);
+      scene.quaternion.copy(rotation);
+      scene.scale.set(customScale, customScale, customScale);
+      scene.updateMatrix();
+      scene.matrixAutoUpdate = false;
+    }
+  }, [transformMatrix]);
+
+  return <primitive object={scene} />;
+};
+
+export default HatModel;
