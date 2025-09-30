8ab357ee93ee20ba5c5eca7b89ba5fbed9ebb3bc
Sofia Borovskaia <sofiaborovskaia@gmail.com>
2025-09-30 12:04:41 +0100

    chore: adapt existing styles to sass format

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./style/index.scss";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
@@ -9,5 +9,5 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
