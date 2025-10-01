7f9cb53acc6e81dad65f59bf8de73218e97b1eba
David Porres <david.porres@frogdesign.com>
2025-10-01 15:05:51 +0200

    feat: add modal examples

--- src/pages/ModalDemo.jsx
+++ src/pages/ModalDemo.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { ModalContext } from "@/context/ModalContext.jsx";
+import MainContainer from "@/pages/ModelViewerPage/index.jsx";
 
 const ModalDemo = () => {
   const [_, setModalData] = useContext(ModalContext);
@@ -8,18 +9,31 @@ const ModalDemo = () => {
     <div style={{ padding: "20px" }}>
       <h1>Modal examples</h1>
 
-      <div>
+      <div style={{ display: "flex", gap: "20px" }}>
         <button
           className="option-button"
           onClick={() =>
             setModalData({
               isOpen: true,
               type: "info",
-              content: "👋 Hola David, esta es tu primera modal!",
+              content: "Esta es una modal con texto simple",
             })
           }
         >
-          Abrir Modal
+          Abrir Modal simple
+        </button>
+
+        <button
+          className="option-button"
+          onClick={() =>
+            setModalData({
+              isOpen: true,
+              type: "info",
+              content: <MainContainer />,
+            })
+          }
+        >
+          Abrir Modal componente React
         </button>
       </div>
     </div>
