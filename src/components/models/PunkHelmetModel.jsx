799c4f8791e7133d18efa20a796211fdaeecad6c
Sofia Borovskaia <sofiaborovskaia@gmail.com>
2025-10-08 15:09:38 +0100

    feat: add two more helmet variants

---
+++ src/components/models/PunkHelmetModel.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useGLTF } from "@react-three/drei";
+import { Matrix4, Vector3, Quaternion } from "three";
+
+const PunkHelmetModel = ({ transformMatrix, visible = true }) => {
+  const { scene } = useGLTF("/assets/models/helmet-punk.glb");
+  const customScale = 7.4;
+
+  useEffect(() => {
+    if (transformMatrix) {
+      const m = new Matrix4().fromArray(transformMatrix);
+      const position = new Vector3();
+      const rotation = new Quaternion();
+      const scale = new Vector3();
+
+      m.decompose(position, rotation, scale);
+
+      scene.position.copy(position);
+      scene.quaternion.copy(rotation);
+      scene.scale.set(customScale, customScale, customScale);
+      scene.updateMatrix();
+      scene.matrixAutoUpdate = false;
+    }
+  }, [transformMatrix]);
+
+  return <primitive object={scene} visible={visible} />;
+};
+
+export default PunkHelmetModel;
--- src/components/product-page-modals/HelmetModalContent/index.jsx
+++ src/components/product-page-modals/HelmetModalContent/index.jsx
@@ -1,14 +1,25 @@
 import { useEffect, useRef, useState } from "react";
-import { getFaceLandmarker } from "/lib/mediapipe";
+// Components
 import ThreeScene from "./ThreeScene";
-import HelmetModel from "../models/HelmetModel";
-import styles from "./Helmet3DViewer.module.scss";
+import HelmetModel from "../../models/HelmetModel";
+import FantasyHelmetModel from "../../models/FantasyHelmetModel";
+import PunkHelmetModel from "../../models/PunkHelmetModel";
+// UI
+import OptionButton from "../../../ui/OptionButton";
+// Dependencies
+import { getFaceLandmarker } from "/lib/mediapipe";
+import styles from "./index.module.scss";
+// Constants
+const DEFAULT_VARIANT = "Default";
+const FANTASY_VARIANT = "Fantasy";
+const PUNK_VARIANT = "Punk";
 
 const Helmet3DViewer = () => {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
 
   const [transformMatrix, setTransformMatrix] = useState(null);
+  const [selectedVariant, setSelectedVariant] = useState(DEFAULT_VARIANT);
 
   useEffect(() => {
     let stream = null;
@@ -72,6 +83,29 @@ const Helmet3DViewer = () => {
 
   return (
     <div className={styles.productPageModalContainer}>
+      <ul className="options-list">
+        <li>
+          <OptionButton
+            label={"Default"}
+            selected={selectedVariant === DEFAULT_VARIANT}
+            onClick={() => setSelectedVariant(DEFAULT_VARIANT)}
+          />
+        </li>
+        <li>
+          <OptionButton
+            label={"Fantasy"}
+            selected={selectedVariant === FANTASY_VARIANT}
+            onClick={() => setSelectedVariant(FANTASY_VARIANT)}
+          />
+        </li>
+        <li>
+          <OptionButton
+            label={"Punk"}
+            selected={selectedVariant === PUNK_VARIANT}
+            onClick={() => setSelectedVariant(PUNK_VARIANT)}
+          />
+        </li>
+      </ul>
       <div className="position-relative">
         {/* Camera recording */}
         <video
@@ -79,6 +113,7 @@ const Helmet3DViewer = () => {
           style={{
             borderRadius: 12,
             transform: "scaleX(-1)",
+            display: "flex",
           }}
         />
 
@@ -91,8 +126,14 @@ const Helmet3DViewer = () => {
           style={{ display: "none" }}
         />
 
-        {/* 3D Model */}
-        <ThreeScene transformMatrix={transformMatrix} model={<HelmetModel />} />
+        {/* 3D Models */}
+        <ThreeScene
+          transformMatrix={transformMatrix}
+          defaultModel={<HelmetModel />}
+          fantasyModel={<FantasyHelmetModel />}
+          punkModel={<PunkHelmetModel />}
+          selectedVariant={selectedVariant}
+        />
       </div>
     </div>
   );
